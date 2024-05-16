87cd4219db4f2f38a3ab604b6b1a7c94656ff6c8
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-16 19:26:16 +0530

    Feat: Reactcomponent using createElement.

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world using react"
-);
+const heading = React.createElement("h1", { id: "heading" }, [
+  "Hello world using react",
+  React.createElement("h2", { id: "parent" }, [
+    "welcome to React learning",
+    React.createElement("p", { id: "child" }, "React elememt"),
+    React.createElement("p", { id: "child2" }, "React DOM"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
