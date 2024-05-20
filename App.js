5005a6acba512a0b1c4388dcc4f50557ab2e247f
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-20 11:28:45 +0530

    Remove:React.createElement .

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, [
-  "Hello world using react",
-  React.createElement("h2", { id: "parent" }, [
-    "welcome to React learning",
-    React.createElement("p", { id: "child" }, "React elememt"),
-    React.createElement("p", { id: "child2" }, "React DOM"),
-  ]),
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(headings);
