e51244a2a0e9bfef0d9f65f495002f3711d74ac8
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-20 13:06:00 +0530

    Feat:React functional component.

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// ** React element using  Jsx
-const headings = <h1 id="heading">Learn React</h1>;
+// * React element using  Jsx
+const headings = <h1>Learn React</h1>;
+
+// *React fun component
+
+// const Headings = () => <h1 id="heading">React component using jsx</h1>;
+
+function Headings() {
+  <h1 id="heading">React component using jsx</h1>;
+}
+
+// *component composition
+const BodyContainer = () => (
+  <>
+    <Headings />
+    {headings}
+    <div>Hello Welcome to react.Here is the React function component</div>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(headings);
+root.render(<BodyContainer />);
