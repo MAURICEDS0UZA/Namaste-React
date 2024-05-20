4fd90a9e62f0a00ebb6622416c661f9ae545d1d2
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-20 11:36:26 +0530

    Added: style to element.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // ** React element using  Jsx
-const headings = <h1>Learn React</h1>;
+const headings = <h1 id="heading">Learn React</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(headings);
