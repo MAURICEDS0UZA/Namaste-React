a3b1e38ef5267fead33bc338ed886d468cbca046
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-28 21:59:36 +0530

    Del:Removed delete component.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
-import FoodContainer from "./components/FoodContainer";
-import { resLists } from "./utils/constants";
-
-const BodyContainer = () => {
-  const [foodLists, setFoodLists] = useState(resLists);
-
-  return (
-    <div className="bodyContainer">
-      <div className="search">
-        <input type="text"></input>
-        <button>Search</button>
-        <button
-          onClick={() =>
-            setFoodLists(
-              foodLists.filter((item) => item.info.avgRating === 4.4)
-            )
-          }
-        >
-          Top Rated
-        </button>
-      </div>
-      <div className="food-list">
-        {foodLists.map((item) => {
-          return <FoodContainer key={item.info.id} resLists={item} />;
-        })}
-      </div>
-    </div>
-  );
-};
+import BodyContainer from "./components/BodyContainer";
 
 const App = () => {
   return (
