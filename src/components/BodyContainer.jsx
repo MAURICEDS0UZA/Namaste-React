42762eefef85fb7fc418ad5ca1a726d64a388784
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-29 17:11:55 +0530

    Feat:Implemented search filter.

--- src/components/BodyContainer.jsx
+++ src/components/BodyContainer.jsx
@@ -5,18 +5,25 @@ import Shimmer from "./Shimmer";
 
 const BodyContainer = () => {
   const [foodLists, setFoodLists] = useState([]);
-  console.log(foodLists.length);
+  const [filterFood, setfilterFood] = useState([]);
+  const [searchVal, setsearchVal] = useState("");
+
   useEffect(() => {
     fetchData();
   }, []);
 
   async function fetchData() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const res = await data.json();
 
-    // setFoodLists(res.data.cards[0].card.card.imageGridCards.info);
+    setfilterFood(
+      res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setFoodLists(
+      res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
   }
 
   return (
@@ -29,8 +36,26 @@ const BodyContainer = () => {
       ) : (
         <>
           <div className="search">
-            <input type="text"></input>
-            <button>Search</button>
+            <input
+              type="text"
+              onChange={(e) => {
+                setsearchVal(e.target.value);
+              }}
+              value={searchVal}
+            ></input>
+            <button
+              onClick={() => {
+                setfilterFood(
+                  foodLists.filter((item) =>
+                    item.info.name
+                      .toLocaleLowerCase()
+                      .includes(searchVal.toLocaleLowerCase())
+                  )
+                );
+              }}
+            >
+              Search
+            </button>
             <button
               onClick={() =>
                 setFoodLists(
@@ -42,7 +67,7 @@ const BodyContainer = () => {
             </button>
           </div>
           <div className="food-list">
-            {foodLists.map((item) => {
+            {filterFood.map((item) => {
               return <FoodContainer key={item?.info?.id} resLists={item} />;
             })}
           </div>
