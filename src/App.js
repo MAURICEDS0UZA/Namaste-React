73edf93ed0383f01b6e0a176419e04e950db1875
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-01 16:46:15 +0530

    Feat:New component added in App.js(about,ErrorBoundary & contact)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import BodyContainer from "./components/BodyContainer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import AboutUs from "./components/AboutUs";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
     <>
       <div className="app-container">
         <Header />
-        <BodyContainer />
+        <Outlet />
       </div>
     </>
   );
 };
 
+// * Routing Into app
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <BodyContainer />,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      // {
+      //   path: "*",
+      //   element: <ErrorBoundary />,
+      // },
+    ],
+    errorElement: <ErrorBoundary />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// *Router provider
+root.render(<RouterProvider router={appRoute}></RouterProvider>);
