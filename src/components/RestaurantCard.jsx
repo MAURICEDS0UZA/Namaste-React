7224da8d84023106c2a121d5ebeea887b8be4140
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-01 20:05:14 +0530

    New:Restaurant card component.

---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import RestaurantDetails from "./RestaurantDetails";
+import { Restaurant_Api } from "../utils/constants";
+
+const RestaurantCard = () => {
+  const [restaurant, setRestaurant] = useState([]);
+  const [restMenu, setrestMenu] = useState([]);
+  const { resId } = useParams();
+
+  useEffect(() => {
+    fetchRes();
+  }, []);
+
+  //  * fetch Restaurant details
+  const fetchRes = async () => {
+    const data = await fetch(Restaurant_Api + resId);
+    const res = await data.json();
+    setRestaurant(res?.data?.cards[2]?.card?.card?.info);
+    setrestMenu(
+      res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card?.itemCards
+    );
+  };
+
+  return (
+    <>
+      <RestaurantDetails resListDetail={restaurant} />
+      {/* Api is response is not consistant */}
+      {/* <RestaurantMenu resMenuDetails={restMenu} /> */}
+    </>
+  );
+};
+
+export default RestaurantCard;
