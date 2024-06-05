f7dbfa70745abc8dfb0a1e06819e5ededb65fc23
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-05 22:41:19 +0530

    Update:veg logo width changed.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1574,9 +1574,9 @@ const star = (
 
 const Veg_Logo = (
   <svg
-    width="100"
-    height="100"
-    viewBox="0 0 100 100"
+    width="20"
+    height="10"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
