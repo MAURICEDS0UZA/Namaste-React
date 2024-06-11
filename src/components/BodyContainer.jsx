534bad5b6cb460d4b43f26fed1aa71e3a9602866
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-12 00:17:47 +0530

    Feat:Body container styled using Tailwind

--- src/components/BodyContainer.jsx
+++ src/components/BodyContainer.jsx
@@ -27,15 +27,16 @@ const BodyContainer = () => {
   };
 
   return (
-    <div className="bodyContainer">
+    <div>
       {foodLists.length === 0 ? (
         <>
           <Shimmer />
         </>
       ) : (
         <>
-          <div className="search">
+          <div className="flex justify-center shadow-md  shadow-gray-400 ">
             <input
+              className="border-2 px-4 m-4 "
               type="text"
               onChange={(e) => {
                 setsearchVal(e.target.value);
@@ -43,6 +44,7 @@ const BodyContainer = () => {
               value={searchVal}
             ></input>
             <button
+              className="bg-orange-200 px-4 py-1 rounded-md m-4 hover:bg-orange-300  "
               onClick={() => {
                 setfilterFood(
                   foodLists.filter((item) =>
@@ -56,18 +58,18 @@ const BodyContainer = () => {
               Search
             </button>
             <button
+              className="bg-orange-200 px-4 py-1 rounded-md m-4  hover:bg-orange-300  "
               onClick={() => {
                 const restaurantsMost = foodLists.filter(
                   (res) => res.info.avgRating > 4.1
                 );
-                console.log("most rated");
                 setfilterFood(restaurantsMost);
               }}
             >
               Top Rated
             </button>
           </div>
-          <div className="food-list">
+          <div className="mx-4 flex gap-4 flex-wrap  m-4 lg:p-4 bg-gray-100 shadow-slate-400 shadow-lg rounded-lg">
             {filterFood.map((item) => {
               return (
                 <Link key={item?.info?.id} to={"/restaurant/" + item?.info?.id}>
