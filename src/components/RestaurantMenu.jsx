085d229d71e0f08fa3f0bba1f315273b69a433ea
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-12 22:51:35 +0530

    Feat:Restaurant Menu style using tailwind.

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -4,8 +4,8 @@ import { Fragment } from "react";
 const RestaurantMenu = ({ resMenuDetails }) => {
   return (
     <>
-      <section className="restaurant-menu">
-        <h2>Recommened</h2>
+      <section className=" p-4 border-t-2 bg-gray-100  rounded-lg w-9/12 grid m-auto">
+        <h2 className="font-bold text-lg">Recommened</h2>
         {resMenuDetails.map((item) => {
           const {
             id,
@@ -17,17 +17,21 @@ const RestaurantMenu = ({ resMenuDetails }) => {
             imageId,
             description,
           } = item?.card?.info;
-          console.log(id);
+
           return (
             <Fragment key={id}>
-              <div key={id}>
-                <div>
-                  <div>{isVeg ? Veg_Logo : Non_Veg_Logo}</div>
-                  <div>{name}</div>
-                  <div>Rs.{defaultPrice / 100 || price / 100}</div>
-                  <div>
+              <div className="flex justify-around ">
+                <div className="flex flex-col  border-white border-t-2 py-4">
+                  <div className="flex items-center pl-4 py-1">
+                    <div>{isVeg ? Veg_Logo : Non_Veg_Logo}</div>
+                    <div className="font-semibold">{name}</div>
+                  </div>
+                  <div className="pl-4 py-1">
+                    Rs.{defaultPrice / 100 || price / 100}
+                  </div>
+                  <div className=" flex items-center  py-1">
                     <svg
-                      width="20"
+                      width="40"
                       height="10"
                       viewBox="0 0 24 24"
                       xmlns="http://www.w3.org/2000/svg"
@@ -37,16 +41,19 @@ const RestaurantMenu = ({ resMenuDetails }) => {
                         points="12,2 15,9 22,9 17,14 18.5,21 12,17 5.5,21 7,14 2,9 9,9"
                       />
                     </svg>
-                    {ratings?.aggregatedRating?.rating}
+                    <span> {ratings?.aggregatedRating?.rating}</span>
                   </div>
-                  <div>{description}</div>
+                  <div className="pl-4 py-1">{description}</div>
                 </div>
                 <div>
-                  <div>
-                    <img src={Food_Image_Menu + imageId} alt="Food image"></img>
+                  <div className="m-4 object-fill">
+                    <img
+                      className="rounded-md "
+                      src={Food_Image_Menu + imageId}
+                      alt="Food image"
+                    ></img>
                   </div>
                 </div>
-                <hr />
               </div>
             </Fragment>
           );
