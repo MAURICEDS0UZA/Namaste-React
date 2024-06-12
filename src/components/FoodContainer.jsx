82998183ed3fe4224e65eb823a353abd8a880a79
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-12 22:54:03 +0530

    Update:FoodContainer using tailwind.

--- src/components/FoodContainer.jsx
+++ src/components/FoodContainer.jsx
@@ -6,13 +6,17 @@ const FoodContainer = ({ resLists }) => {
 
   return (
     <>
-      <div className="food-container">
-        <img src={Food_Log + cloudinaryImageId}></img>
-        <div>
+      <div className="flex flex-wrap w-48 p-4 m-4  rounded-lg h-96  hover:bg-white bg-gray-300">
+        <img className="rounded-lg" src={Food_Log + cloudinaryImageId}></img>
+        <div className="flex flex-col">
           <p>
             <b>{name}</b>
           </p>
-          <p>{cuisines.join(" ,")}</p>
+          {cuisines.length > 3 ? (
+            <p>{cuisines.slice(0, 2).join(", ")}....</p>
+          ) : (
+            <p>{cuisines.join(", ")}</p>
+          )}
           <p>{avgRating} Stars</p>
           <p>{costForTwo} </p>
           <p>{sla?.deliveryTime} minutes</p>
