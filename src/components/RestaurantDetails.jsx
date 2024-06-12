4c17cf43ffd0d82dd5a85010e06690faff520ebd
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-12 22:53:13 +0530

    Update:Restaurant details using tailwind.

--- src/components/RestaurantDetails.jsx
+++ src/components/RestaurantDetails.jsx
@@ -13,21 +13,23 @@ const RestaurantDetails = ({ resListDetail }) => {
 
   return (
     <>
-      <section className="restaurant-detail">
-        <h1 className="restaurant-name">{name}</h1>
-        <div className="restaurant-detail_list">
-          <div className="restaurant-detail_rating">
+      <section className="p-4 w-9/12 m-auto my-4  bg-gray-100 flex flex-col rounded-xl">
+        <h1 className="font-bold text-2xl py-4">{name}</h1>
+        <div className="flex flex-col bg-slate-50  border-2 border-gray-200 rounded-xl">
+          <div className="flex flex-row px-4 py-2">
             {star}
             <div>
-              {avgRating} {totalRatingsString}
-              <span>.</span>
+              {" "}
+              {avgRating} ({totalRatingsString})<span className="px-1">.</span>
             </div>
-            <div>{costForTwoMessage}</div>
+            <div className="px-2">{costForTwoMessage}</div>
           </div>
-          <div>{cuisines?.join(" ,  ")}</div>
-          <div>{areaName}</div>
+          <div className="font-bold underline text-orange-400 px-4 py-2">
+            {cuisines?.join(" ,  ")}
+          </div>
+          <div className="px-4 py-2">{areaName}</div>
           <hr />
-          <div className="restaurant-detail-delivery">
+          <div className="px-4 py-2 flex gap-2">
             <div>{sla?.lastMileTravelString} |</div>
             <div>{sla?.slaString}</div>
           </div>
