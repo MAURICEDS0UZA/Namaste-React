11ca0816478e0d7915d41e9606ea2bf56831e590
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-14 15:53:04 +0530

    Feat:Animation for shimmer.

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,18 +2,18 @@ const Shimmer = () => {
   return (
     <>
       <div className="bg-gray-50 flex flex-wrap  mx-10 p-8 rounded-lg gap-y-2 justify-center">
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
-        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse "></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse "></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
+        <div className="m-2  px-24 py-36 bg-gray-200 shadow-lg shadow-gray-400 rounded-lg animate-pulse"></div>
       </div>
     </>
   );
