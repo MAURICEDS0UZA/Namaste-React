039ca410587eb7e15288c48d66705483d5bb6315
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-30 00:39:49 +0530

    Feat:Test case for Header.

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,10 +1,57 @@
 import { screen } from "@testing-library/dom";
-import Header from "../Header";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import Header from "../Header";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
 
-test("render heading", () => {
-  render(<Header />);
-  const heading = screen.getAllByRole("heading");
-  expect(heading).toBeInTheDocument();
+beforeAll(() => console.log("beforeAll "));
+afterAll(() => console.log("afterAll "));
+afterEach(() => console.log("aftereach "));
+beforeEach(() => console.log("before each "));
+
+describe("should combine All test case", () => {
+  test("Should render header component with home", () => {
+    render(
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    const homebtn = screen.getByRole("link", { name: "Home" });
+    expect(homebtn).toBeInTheDocument();
+  });
+
+  test("should render login button", () => {
+    render(
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </Provider>
+    );
+    const loginBtn = screen.getByRole("button", { name: "Log in" });
+
+    expect(loginBtn).toHaveTextContent("Log in");
+  });
+
+  test("should render logout button on click of login", () => {
+    render(
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </Provider>
+    );
+    const loginBtn = screen.getByText("Log in");
+
+    expect(loginBtn).toBeInTheDocument();
+    // click on login btn
+    fireEvent.click(loginBtn);
+    const logOutBtn = screen.getByRole("button", { name: "Log out" });
+    expect(logOutBtn).toBeInTheDocument();
+  });
 });
