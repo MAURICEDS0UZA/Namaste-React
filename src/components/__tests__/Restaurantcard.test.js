e43d3583a57a52f6121dcd623e5c8b5e3c4313e7
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-07-02 00:25:45 +0530

    Feat:Test Case for promoted rest card.

--- src/components/__tests__/Restaurantcard.test.js
+++ src/components/__tests__/Restaurantcard.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import FoodContainer from "../FoodContainer";
+import FoodContainer, { withOpenRestaurantCard } from "../FoodContainer";
 import resLists from "../mocks/foodContainer.json";
-import RestaurantPromoted from "../RestaurantCard";
 
 test("should render restaurant card in Body component", () => {
   render(<FoodContainer resLists={resLists} />);
@@ -10,4 +9,13 @@ test("should render restaurant card in Body component", () => {
   const cardName = screen.getByText("Wow! Momo");
 
   expect(cardName).toBeInTheDocument();
+});
+
+test("should render promoted restaurant card", () => {
+  const RestaurantPromoted = withOpenRestaurantCard(FoodContainer);
+  render(<RestaurantPromoted resLists={resLists} />);
+  // searching Promoted text
+  const Promoted = screen.getByText("Promoted");
+  // Asseration
+  expect(Promoted).toBeInTheDocument();
 });
